00573f7d8dd4173a8e857378b060b5b3dfc3881b
Ruben Rasmussen <rube0265@videndjurs.net>
2015-12-16 11:46:02 +0100

    jkknjk

--- NikonEksperten/Controllers/HomeController.cs
+++ NikonEksperten/Controllers/HomeController.cs
@@ -10,18 +10,39 @@ namespace NikonEksperten.Controllers
 {
     public class HomeController : Controller
     {
-        facKategori fkat = new facKategori();
+        private facKategori fkat = new facKategori();
+        private facProdukter fp = new facProdukter();
         // GET: Home
         public ActionResult Index()
         {
             return View(fkat.GetForsideProdukter());
         }
+
         public ActionResult Kategorier()
         {
             return View(fkat.GetAll());
         }
 
-        
+        public ActionResult SoegeResultater(List<Produkter> pl)
+        {
+            return View(pl);
+        }
+        [HttpPost]
+        public ActionResult SoegProdukter(int KatID, int Maxpris, string SoegeOrd)
+        {
 
+            if (ModelState.IsValid)
+            {
+                List<Produkter> pl = new List<Produkter>();
+
+                foreach (Produkter produkt in fp.AdvSearch(KatID, Maxpris, SoegeOrd))
+                {
+                    pl.Add(produkt);
+                }
+                ViewBag.SoegError = "Korrekt Input";
+                return View("SoegeResultater", pl);
+            }
+            return View("Index");
+        }
     }
 }
