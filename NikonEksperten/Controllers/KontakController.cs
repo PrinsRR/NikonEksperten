15cd3a01ac0968d24003bd9ea91a74503a58c859
Kenneth Petersen <kenn187e@videndjurs.net>
2015-12-17 12:54:53 +0100

    yy

---
+++ NikonEksperten/Controllers/KontakController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NikonRepo.Factories;
+
+namespace NikonEksperten.Controllers
+{
+    public class KontakController : Controller
+    {
+        private facKontakt fkon = new facKontakt();
+        // GET: Kontak
+        public ActionResult Index()
+        {
+            return View(fkon.Get(1));
+        }
+    }
+}
